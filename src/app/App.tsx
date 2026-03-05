3322445c7fe89f40c89e762973f6bb2a87f506a4
michalstencel <sttencel@gmail.com>
2026-03-05 12:47:24 +0100

    v1

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { PongGame } from "./components/PongGame";
+
+export default function App() {
+  return (
+    <div className="size-full flex items-center justify-center bg-gray-950">
+      <PongGame />
+    </div>
+  );
+}

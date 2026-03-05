e834a8a90114b4fad179dcd86ae279c39bc23200
michalstencel <sttencel@gmail.com>
2026-03-05 14:36:31 +0100

    vision

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { PongGame } from "./components/PongGame";
-
-export default function App() {
-  return (
-    <div className="size-full flex items-center justify-center bg-gray-950">
-      <PongGame />
-    </div>
-  );
-}
